aa99c536e04acd9f05db8917b3cca9c9d4ccca2d
Garrett Cullen <Garth619@users.noreply.github.com>
2020-02-11 13:05:36 -0800

    push

--- wp-content/themes/torgenson/page-templates/homepage_template_parts/section-1.php
+++ wp-content/themes/torgenson/page-templates/homepage_template_parts/section-1.php
@@ -1,7 +1,48 @@
 <section id="section_one">
 	
+	<div class="sec_one_top">
 		
-
+		<div class="sec_one_top_inner">
+			
+			<div class="sec_one_top_row">
+				
+				<div class="sec_one_top_row_left">
+					
+					<span class="sec_one_subheader">phoenix personal injury attorneys</span><!-- sec_one_subheader -->
+					
+					<span class="sec_one_largeheader">100% dedicated to helping good people who are hurt.</span><!-- sec_one_largeheader -->
+					
+					<a class="request_button" href="#consultation">
+						
+						<span>Request Free Consultation</span>
+					
+					</a><!-- request_button -->
+					
+				</div><!-- sec_one_top_row_left -->
+				
+				<div class="sec_one_top_row_right"></div><!-- sec_one_top_row_right -->
+				
+			</div><!-- sec_one_top_row -->
+			
+		</div><!-- sec_one_top_inner -->
+		
+		<picture>
+		
+			<img class="hero" src="<?php bloginfo('template_directory');?>/images/hero-desktop.jpg"/>
+		
+		</picture>
+		
+	</div><!-- sec_one_top -->
+		
+	<div class="sec_one_bottom">
+		
+		<div class="sec_one_bottom_inner">
+			
+			
+			
+		</div><!-- sec_one_bottom_inner -->
+		
+	</div><!-- sec_one_bottom -->
 	
 </section><!-- section_one -->
 
