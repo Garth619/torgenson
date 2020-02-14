aa1a735cbac16f35f2aa42b7584a43fbffef133d
Garrett Cullen <Garth619@users.noreply.github.com>
2020-02-13 16:58:42 -0800

    push

---
+++ wp-content/themes/torgenson/page-templates/homepage_template_parts/section-3.php
@@ -0,0 +1,31 @@
+<section id="section_three">
+	
+	<div class="sec_three_inner">
+		
+		<div class="sec_three_left">
+			
+			<span class="sec_three_cr_title">Client Success</span><!-- sec_three_cr_title -->
+			
+			<a class="sec_three_button" href="">
+				
+				<span>View All Results</span>
+				
+				<?php echo file_get_contents("wp-content/themes/torgenson/images/arrow.svg"); ?>
+				
+			</a><!-- sec_three_button -->
+			
+		</div><!-- sec_three_left -->
+		
+		<div class="sec_three_right">
+			
+			<div class="sec_three_cr">
+				
+				
+				
+			</div><!-- sec_three_cr -->
+			
+		</div><!-- sec_three_right -->
+		
+	</div><!-- sec_three_inner -->
+	
+</section><!-- section_three -->
