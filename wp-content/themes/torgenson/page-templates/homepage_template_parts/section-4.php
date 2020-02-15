d68fb59bbf1690b5a09cf628a1978499998b162f
Garrett Cullen <Garth619@users.noreply.github.com>
2020-02-14 16:47:13 -0800

    push

---
+++ wp-content/themes/torgenson/page-templates/homepage_template_parts/section-4.php
@@ -0,0 +1,63 @@
+<section id="section_four">
+	
+	<div class="sec_four_inner">
+		
+		<div class="sec_four_left">
+			
+			<span class="sec_four_title">Why Hire A Personal Injury Attorney?</span><!-- sec_four_title -->
+			
+			<div class="sec_four_content content">
+				
+				<h2>Get fully and fairly compensated</h2>
+
+				<p>While establishing economic losses that have already happened (like medical bills and property damage) is a relatively straightforward process, the same is not true for future losses or noneconomic injuries like physical and emotional pain or suffering. The assistance of an attorney will ensure that you get the compensation to which you are legally entitled.</p>
+
+				<h2>Defend your legal rights</h2>
+
+				<p>If you mistakenly say something that indicates partial liability, it could significantly affect the amount of compensation you end up receiving. When you retain a lawyer, he or she will communicate with the insurance company or the adverse party on your behalf, ensuring that you do not say or do anything to hurt your case.</p>
+
+				<h2>Navigate tricky insurance negotiation</h2>
+
+				<p>Insurance companies are sophisticated parties that are in the business of denying claims and undoubtedly have an army of skilled insurance defense lawyers ready to litigate a case at the drop of a hat. As an accident victim, you need to prepared to take your case to trial, and Torgenson Law can help.</p>
+				
+			</div><!-- sec_four_content -->
+			
+		</div><!-- sec_four_left -->
+		
+		<div class="sec_four_right">
+			
+			<div class="sec_four_img_wrapper">
+				
+				<div class="sec_four_img_left">
+				
+					<div class="sec_four_img sec_four_img_one">
+					
+						<img src="<?php bloginfo('template_directory');?>/images/content-image-1.jpg"/>
+					
+					</div><!-- sec_four_img -->
+				
+				</div><!-- sec_four_img_left -->
+				
+				<div class="sec_four_img_right">
+				
+					<div class="sec_four_img sec_four_img_two">
+					
+						<img src="<?php bloginfo('template_directory');?>/images/content-image-2.jpg"/>
+					
+					</div><!-- sec_four_img -->
+				
+					<div class="sec_four_img sec_four_img_three">
+					
+						<img src="<?php bloginfo('template_directory');?>/images/content-image-3.jpg"/>
+					
+					</div><!-- sec_four_img -->
+				
+				</div><!-- sec_four_img_right -->
+				
+			</div><!-- sec_four_img_wrapper -->
+			
+		</div><!-- sec_four_right -->
+		
+	</div><!-- sec_four_inner -->
+	
+</section><!-- section_four -->
