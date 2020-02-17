42adbce6fa48f5e0bc10b3c514ddc75bf4ca85ea
Garrett Cullen <Garth619@users.noreply.github.com>
2020-02-17 14:14:44 -0800

    push

--- wp-content/themes/torgenson/footer.php
+++ wp-content/themes/torgenson/footer.php
@@ -20,13 +20,15 @@
 		
 		<div class="location_inner">
 			
+			<div class="location_left">
+			
 			<div class="location_col">
 				
 				<span class="location_title">Torgenson Law</span><!-- location_title -->
 				
 				<span class="location_header">Address</span><!-- location_header -->
 				
-				<span class="location_content">333 West Roosevelt Street<br/> Phoenix, AZ 85003</span><!-- location_content -->
+				<span class="location_content">333 West Roosevelt Street<br/> footer_phoenix, AZ 85003</span><!-- location_content -->
 				
 				<a class="get_directions_button" href="">
 				
@@ -40,13 +42,73 @@
 			
 			<div class="location_col">
 				
+				<span class="location_header">Phone Number</span><!-- location_header -->
 				
+				<a class="location_content" href="tel:6028332774">602.833.2774</a><!-- location_content -->
+				
+				<span class="location_header office_hours_title">Office Hours</span><!-- location_header -->
+				
+				<span class="location_content">Mon-Fri 8:00am - 5:00pm</span><!-- location_content -->
+				
+				<span class="location_header social_title">Social</span><!-- location_header -->
+				
+				<div class="social_wrapper">
+					
+					<a class="sm" href="">
+						
+						<?php echo file_get_contents("wp-content/themes/torgenson/images/facebook.svg"); ?>
+						
+					</a>
+					
+					<a class="sm" href="">
+						
+						<?php echo file_get_contents("wp-content/themes/torgenson/images/twitter.svg"); ?>
+						
+					</a>
+					
+					<a class="sm" href="">
+						
+						<?php echo file_get_contents("wp-content/themes/torgenson/images/linkedin.svg"); ?>
+						
+					</a>
+					
+				</div><!-- social_wrapper -->
 				
 			</div><!-- location_col -->
 			
+			</div><!-- location_left -->
+			
 		</div><!-- location_inner -->
 		
+		<div class="footer_phoenix_cutout">
+			
+			<div class="footer_phoenix_spacer footer_phoenix_spacer_one"></div><!-- footer_phoenix_spacer -->
+			
+			<img class="footer_phoenix_cutout_img" src="<?php bloginfo('template_directory');?>/images/pheonix-cutout-2.svg"/>
+			
+		</div><!-- footer_phoenix_cutout -->
+		
 	</div><!-- location_info -->
+	
+	<div class="copyright_wrapper">
+		
+		<div class="copyright_inner">
+			
+			<ul>
+				<li>Copyright <?php echo date("Y"); ?> &copy; Torgenson Law</li>
+				<li><a href="">Privacy Policy</a></li>
+				<li><a href="">Disclaimer</a></li>
+			</ul>
+			
+			<a class="ilawyer" href="//ilawyermarketing.com" target="_blank" rel="noopener">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/ilawyer-logo.svg"/>
+				
+			</a><!-- ilawyer -->
+			
+		</div><!-- copyright_inner -->
+		
+	</div><!-- copyright_wrapper -->
 		
 </footer>
 
