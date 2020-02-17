2d6742f67d8829c342d0017c553ab2df9acaaac9
Garrett Cullen <Garth619@users.noreply.github.com>
2020-02-17 13:16:04 -0800

    push

--- wp-content/themes/torgenson/footer.php
+++ wp-content/themes/torgenson/footer.php
@@ -16,7 +16,37 @@
 		
 	</div><!-- form_wrapper -->
 	
-
+	<div class="location_info">
+		
+		<div class="location_inner">
+			
+			<div class="location_col">
+				
+				<span class="location_title">Torgenson Law</span><!-- location_title -->
+				
+				<span class="location_header">Address</span><!-- location_header -->
+				
+				<span class="location_content">333 West Roosevelt Street<br/> Phoenix, AZ 85003</span><!-- location_content -->
+				
+				<a class="get_directions_button" href="">
+				
+					<span>Directions</span>
+				
+					<?php echo file_get_contents("wp-content/themes/torgenson/images/arrow.svg"); ?>
+				
+				</a><!-- get_directions_button -->
+				
+			</div><!-- location_col -->
+			
+			<div class="location_col">
+				
+				
+				
+			</div><!-- location_col -->
+			
+		</div><!-- location_inner -->
+		
+	</div><!-- location_info -->
 		
 </footer>
 
