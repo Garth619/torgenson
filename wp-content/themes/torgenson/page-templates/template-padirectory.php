a4be3dc4927a66112315b2c64bf41f1dc8a189c5
Garrett Cullen <Garth619@users.noreply.github.com>
2020-02-18 17:24:36 -0800

    push

--- wp-content/themes/torgenson/page-templates/template-padirectory.php
+++ wp-content/themes/torgenson/page-templates/template-padirectory.php
@@ -4,9 +4,26 @@
 
 get_header(); ?>
 
-
+	<div id="internal_main">
 		
-		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+		<?php echo file_get_contents("wp-content/themes/torgenson/images/pheonix-test.svg"); ?>
+	
+	<div class="page_container">
+		
+		<h1 class="internal_title"><?php the_title();?></h1><!-- internal_title -->
+		
+		<div class="pa_directory_wrapper">
+			
+			<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+			
+		</div><!-- pa_directory_wrapper -->
+		
+	</div><!-- page_container -->
+	
+	
+</div><!-- internal_main -->
+		
+		
 
 
 <?php get_footer(); ?>
