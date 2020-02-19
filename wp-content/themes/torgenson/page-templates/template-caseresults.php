b2f22637cb5d94bbe093f56102f29832c1f1ffe2
Garrett Cullen <Garth619@users.noreply.github.com>
2020-02-19 13:38:34 -0800

    push

--- wp-content/themes/torgenson/page-templates/template-caseresults.php
+++ wp-content/themes/torgenson/page-templates/template-caseresults.php
@@ -4,6 +4,71 @@
 
 get_header(); ?>
 
+<div id="internal_main">
+		
+		<?php echo file_get_contents("wp-content/themes/torgenson/images/pheonix-test.svg"); ?>
+	
+	<div class="internal_container">
+		
+		<h1 class="internal_title"><?php the_title();?></h1><!-- internal_title -->
+		
+		<div class="cr_wrapper">
+			
+			<div class="single_cr">
+				
+				<span class="cr_type">Personal Injury</span><!-- cr_type -->
+				
+				<span class="cr_amount">$8.25M</span><!-- cr_amount -->
+				
+				<span class="cr_description">Gun injury led to client’s paraplegia</span><!-- cr_description -->
+				
+			</div><!-- single_cr -->
+			
+			<div class="single_cr">
+				
+				<span class="cr_type">Personal Injury</span><!-- cr_type -->
+				
+				<span class="cr_amount">$8.25M</span><!-- cr_amount -->
+				
+				<span class="cr_description">Gun injury led to client’s paraplegia</span><!-- cr_description -->
+				
+			</div><!-- single_cr -->
+			
+			<div class="single_cr">
+				
+				<span class="cr_type">Personal Injury</span><!-- cr_type -->
+				
+				<span class="cr_amount">$8.25M</span><!-- cr_amount -->
+				
+				<span class="cr_description">Gun injury led to client’s paraplegia</span><!-- cr_description -->
+				
+			</div><!-- single_cr -->
+			
+			<div class="single_cr">
+				
+				<span class="cr_type">Personal Injury</span><!-- cr_type -->
+				
+				<span class="cr_amount">$8.25M</span><!-- cr_amount -->
+				
+				<span class="cr_description">Gun injury led to client’s paraplegia</span><!-- cr_description -->
+				
+			</div><!-- single_cr -->
+			
+			<div class="single_cr">
+				
+				<span class="cr_type">Personal Injury</span><!-- cr_type -->
+				
+				<span class="cr_amount">$8.25M</span><!-- cr_amount -->
+				
+				<span class="cr_description">Gun injury led to client’s paraplegia</span><!-- cr_description -->
+				
+			</div><!-- single_cr -->
+						
+		</div><!-- cr_wrapper -->
+		
+	</div><!-- internal_container -->
+	
+</div><!-- internal_main -->
 
 						
 			  
