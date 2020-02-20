636020930e4397054e9dff863c1eddad85c4c0f7
Garrett Cullen <Garth619@users.noreply.github.com>
2020-02-19 18:02:41 -0800

    push

--- wp-content/themes/torgenson/page-templates/template-meetteam.php
+++ wp-content/themes/torgenson/page-templates/template-meetteam.php
@@ -4,7 +4,121 @@
 
 get_header(); ?>
 
-
+<div id="internal_main">
+		
+		<?php echo file_get_contents("wp-content/themes/torgenson/images/pheonix-test.svg"); ?>
+	
+	<div class="internal_container">
+		
+		<h1 class="internal_title"><?php the_title();?></h1><!-- internal_title -->
+		
+		<div class="meet_team_wrapper">
+			
+			<div class="single_att">
+				
+				<a href="">
+					
+					<div class="single_att_img">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/attorney-torgenson.jpg"/>
+						
+						<div class="single_att_overlay"></div><!-- single_att_overlay -->
+						
+					</div><!-- single_att_img -->
+					
+					<span class="single_att_name">John Torgenson</span><!-- single_att_name -->
+					
+					<span class="single_att_position">Managing Partner</span><!-- single_att_position -->
+					
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a href="">
+					
+					<div class="single_att_img">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/attorney-torgenson.jpg"/>
+						
+						<div class="single_att_overlay"></div><!-- single_att_overlay -->
+						
+					</div><!-- single_att_img -->
+					
+					<span class="single_att_name">John Torgenson</span><!-- single_att_name -->
+					
+					<span class="single_att_position">Managing Partner</span><!-- single_att_position -->
+					
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a href="">
+					
+					<div class="single_att_img">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/attorney-torgenson.jpg"/>
+						
+						<div class="single_att_overlay"></div><!-- single_att_overlay -->
+						
+					</div><!-- single_att_img -->
+					
+					<span class="single_att_name">John Torgenson</span><!-- single_att_name -->
+					
+					<span class="single_att_position">Managing Partner</span><!-- single_att_position -->
+					
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a href="">
+					
+					<div class="single_att_img">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/attorney-torgenson.jpg"/>
+						
+						<div class="single_att_overlay"></div><!-- single_att_overlay -->
+						
+					</div><!-- single_att_img -->
+					
+					<span class="single_att_name">John Torgenson</span><!-- single_att_name -->
+					
+					<span class="single_att_position">Managing Partner</span><!-- single_att_position -->
+					
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a href="">
+					
+					<div class="single_att_img">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/attorney-torgenson.jpg"/>
+						
+						<div class="single_att_overlay"></div><!-- single_att_overlay -->
+						
+					</div><!-- single_att_img -->
+					
+					<span class="single_att_name">John Torgenson</span><!-- single_att_name -->
+					
+					<span class="single_att_position">Managing Partner</span><!-- single_att_position -->
+					
+				</a>
+				
+			</div><!-- single_att -->
+			
+		</div><!-- meet_team_wrapper -->
+		
+	</div><!-- internal_container -->
+	
+</div><!-- internal_main -->
 
 
 <?php get_footer(); ?>
