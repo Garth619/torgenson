8575ab116039a8f8ce967019822ea0d0ed7edad6
Garrett Cullen <Garth619@users.noreply.github.com>
2020-02-19 16:16:35 -0800

    push

--- wp-content/themes/torgenson/page-templates/template-video.php
+++ wp-content/themes/torgenson/page-templates/template-video.php
@@ -4,6 +4,212 @@
 
 get_header(); ?>
 
+<div id="internal_main">
+		
+		<?php echo file_get_contents("wp-content/themes/torgenson/images/pheonix-test.svg"); ?>
+	
+	<div class="internal_container">
+		
+		<h1 class="internal_title"><?php the_title();?></h1><!-- internal_title -->
+		
+		<div class="video_wrapper">
+			
+			<div class="single_video">
+				
+				<div class="single_video_thumbnail">
+					
+					<div class="single_video_thumb wisita wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail"></div><!-- mywisita -->
+					
+					<div class="single_video_overlay">
+						
+						<div class="play_button">
+									
+							<span></span>
+									
+						</div><!-- play_button -->
+						
+					</div><!-- single_video_overlay -->
+					
+				</div><!-- single_video_thumbnail -->
+				
+				<div class="video_title_wrapper">
+				
+					<span class="video_title">Firm Overview</span><!-- video_title -->
+				
+				</div><!-- video_title_wrapper -->
+				
+			</div><!-- single_video -->
+			
+			<div class="single_video">
+				
+				<a href="https://www.youtube.com/embed/XBPjVzSoepo" data-lity>
+				
+				<div class="single_video_thumbnail">
+					
+					<div class="single_video_thumb">
+						
+						<img class="youtube" src="https://img.youtube.com/vi/XBPjVzSoepo/0.jpg"/>
+						
+					</div><!-- single_youtube -->
+					
+					<div class="single_video_overlay">
+						
+						<div class="play_button">
+									
+							<span></span>
+									
+						</div><!-- play_button -->
+						
+					</div><!-- single_video_overlay -->
+					
+				</div><!-- single_video_thumbnail -->
+				
+				<div class="video_title_wrapper">
+				
+					<span class="video_title">Firm Overview</span><!-- video_title -->
+				
+				</div><!-- video_title_wrapper -->
+				
+				</a>
+				
+			</div><!-- single_video -->
+			
+			<div class="single_video">
+				
+				<a href="https://www.youtube.com/embed/XBPjVzSoepo" data-lity>
+				
+				<div class="single_video_thumbnail">
+					
+					<div class="single_video_thumb">
+						
+						<img class="youtube" src="https://img.youtube.com/vi/XBPjVzSoepo/0.jpg"/>
+						
+					</div><!-- single_youtube -->
+					
+					<div class="single_video_overlay">
+						
+						<div class="play_button">
+									
+							<span></span>
+									
+						</div><!-- play_button -->
+						
+					</div><!-- single_video_overlay -->
+					
+				</div><!-- single_video_thumbnail -->
+				
+				<div class="video_title_wrapper">
+				
+					<span class="video_title">Firm Overview</span><!-- video_title -->
+				
+				</div><!-- video_title_wrapper -->
+				
+				</a>
+				
+			</div><!-- single_video -->
+			
+			<div class="single_video">
+				
+				<a href="https://www.youtube.com/embed/XBPjVzSoepo" data-lity>
+				
+				<div class="single_video_thumbnail">
+					
+					<div class="single_video_thumb">
+						
+						<img class="youtube" src="https://img.youtube.com/vi/XBPjVzSoepo/0.jpg"/>
+						
+					</div><!-- single_youtube -->
+					
+					<div class="single_video_overlay">
+						
+						<div class="play_button">
+									
+							<span></span>
+									
+						</div><!-- play_button -->
+						
+					</div><!-- single_video_overlay -->
+					
+				</div><!-- single_video_thumbnail -->
+				
+				<div class="video_title_wrapper">
+				
+					<span class="video_title">Firm Overview</span><!-- video_title -->
+				
+				</div><!-- video_title_wrapper -->
+				
+				</a>
+				
+			</div><!-- single_video -->
+			
+			<div class="single_video">
+				
+				<a href="https://www.youtube.com/embed/XBPjVzSoepo" data-lity>
+				
+				<div class="single_video_thumbnail">
+					
+					<div class="single_video_thumb">
+						
+						<img class="youtube" src="https://img.youtube.com/vi/XBPjVzSoepo/0.jpg"/>
+						
+					</div><!-- single_youtube -->
+					
+					<div class="single_video_overlay">
+						
+						<div class="play_button">
+									
+							<span></span>
+									
+						</div><!-- play_button -->
+						
+					</div><!-- single_video_overlay -->
+					
+				</div><!-- single_video_thumbnail -->
+				
+				<div class="video_title_wrapper">
+				
+					<span class="video_title">Firm Overview</span><!-- video_title -->
+				
+				</div><!-- video_title_wrapper -->
+				
+				</a>
+				
+			</div><!-- single_video -->
+			
+			<div class="single_video">
+				
+				<div class="single_video_thumbnail">
+					
+					<div class="single_video_thumb wisita wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail"></div><!-- mywisita -->
+					
+					<div class="single_video_overlay">
+						
+						<div class="play_button">
+									
+							<span></span>
+									
+						</div><!-- play_button -->
+						
+					</div><!-- single_video_overlay -->
+					
+				</div><!-- single_video_thumbnail -->
+				
+				<div class="video_title_wrapper">
+				
+					<span class="video_title">Firm Overview</span><!-- video_title -->
+				
+				</div><!-- video_title_wrapper -->
+				
+			</div><!-- single_video -->
+									
+		</div><!-- video_wrapper -->
+		
+	</div><!-- internal_container -->
+	
+</div><!-- internal_main -->
+
+<script src="https://fast.wistia.net/assets/external/E-v1.js" async></script>
 
 
 <?php get_footer(); ?>
+
