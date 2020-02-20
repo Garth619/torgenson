b9970f32438bb2091d73b3926e29e7a25034dce4
Garrett Cullen <Garth619@users.noreply.github.com>
2020-02-20 13:20:02 -0800

    push

--- wp-content/themes/torgenson/page-templates/template-about.php
+++ wp-content/themes/torgenson/page-templates/template-about.php
@@ -4,12 +4,68 @@
 
 get_header(); ?>
 
-
-
-		
-		<?php get_template_part( 'loop', 'page' ); ?>
-		
+<div id="internal_main">
 	
+	<div class="about_top">
+		
+		<div class="about_top_content_wrapper">
+			
+			<h1 class="about_title"><?php the_title();?></h1><!-- about_title -->
+			
+			<div class="about_top_content">
+				
+				<p>Our team of Phoenix personal injury lawyers will not stop fighting until we uncover every single detail that we need to know. Our investigative process is deeply thorough and effective, and we keep you included in every step of the process so you’re never in the dark. You can trust us to be completely honest with you and to continue defending your rights until the case has been closed. </p>
+				
+			</div><!-- about_top_content -->
+			
+		</div><!-- about_top_content_wrapper -->
+		
+		<img class="about_top_img" src="<?php bloginfo('template_directory');?>/images/internal-about.jpg"/><!-- about_top_img -->
+		
+	</div><!-- about_top -->
+	
+	<div class="about_middle">
+		
+		<div class="about_middle_content">
+			
+			<div class="about_middle_left content">
+				
+				<h2>Get fully and fairly compensated</h2>
 
+				<p>While establishing economic losses that have already happened (like medical bills and property damage) is a relatively straightforward process, the same is not true for future losses or noneconomic injuries like physical and emotional pain or suffering. The assistance of an attorney will ensure that you get the compensation to which you are legally entitled.</p>
 
+				<p>If you mistakenly say something that indicates partial liability, it could significantly affect the amount of compensation you end up receiving. When you retain a lawyer, he or she will communicate with the insurance company or the adverse party on your behalf, ensuring that you do not say or do anything to hurt your case.</p>
+
+				<h2>Navigate tricky insurance negotiation</h2>
+
+				<p>Insurance companies are sophisticated parties that are in the business of denying claims and undoubtedly have an army of skilled insurance defense lawyers ready to litigate a case at the drop of a hat. As an accident victim, you need to prepared to take your case to trial, and Torgenson Law can help.</p>
+				
+			</div><!-- about_middle_left -->
+			
+			<div class="about_middle_right">
+				
+				<div class="about_middle_imge_wrapper">
+					
+					<div class="about_middle_img">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/about-image1.jpg"/>
+						
+					</div><!-- about_middle_img -->
+					
+					<div class="about_middle_img">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/about-image2.jpg"/>
+						
+					</div><!-- about_middle_img -->
+					
+				</div><!-- about_middle_imge_wrapper -->
+				
+			</div><!-- about_middle_right -->
+			
+		</div><!-- about_middle_content -->
+		
+	</div><!-- about_middle -->
+	
+</div><!-- internal_main -->
+		
 <?php get_footer(); ?>
