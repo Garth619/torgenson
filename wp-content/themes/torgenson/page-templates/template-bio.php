1e7feb89a978b12c6b7e96abd1eb857dbe1299ff
Garrett Cullen <Garth619@users.noreply.github.com>
2020-02-19 17:23:25 -0800

    push

--- wp-content/themes/torgenson/page-templates/template-bio.php
+++ wp-content/themes/torgenson/page-templates/template-bio.php
@@ -34,9 +34,40 @@ get_header(); ?>
 					
 					<span class="att_position">Managing Partner</span><!-- att_position -->
 					
+					<div class="att_bio_description">
+						
+						<p>John Torgenson is honored to stick up for good people who are hurt. He grew up in St. George, Utah, where his parents, Fernando and Karen, raised him with a blue-collar work ethic, accountability for decisions, and taught him to always stick up for himself and others that are being bullied.</p>
+						
+												
+					</div><!-- att_bio_description -->
+					
 				</div><!-- att_bio_right -->
 				
 			</div><!-- att_bio_top -->
+			
+			<div class="att_bio_bottom content">
+				
+				<h2>Learn more about John</h2>
+
+				<p>After high school, John attended the University of Utah, graduating in 2001. John then attended his dream school, Notre Dame Law School, where he graduated, with honors, in 2004. John is licensed to fight in court for real people in the State of Arizona, the United States Federal District Court of Arizona, and the 9th Federal Circuit Court of Appeals.</p>
+
+				<p>Before establishing what is now Torgenson Law, John practiced in the litigation group at Fennemore Craig, the oldest law firm in Arizona and one of the largest firms in the Southwest. John will always be grateful to the excellent lawyers and great leaders who gave him a shot and taught him so much about being a lawyer at Fennemore Craig. Having practiced at Fennemore Craig in both the defense and plaintiff’s practice areas gives John a complete perspective of the litigation process, and valuable insight into how to efficiently and effectively advance his clients’ interests. His unique and balanced background enhances his credibility with defense lawyers, insurers, and defendants as well as with judges and arbitrators.</p>
+
+				<p>John realizes that civic and community involvement is essential in order for him to keep a solid balance between his professional and personal life. He was named one of Phoenix’s top 35 entrepreneurs by the Arizona Republic. He has been a board member at Kids @ Core (offering fitness and healthy living classes for underprivileged children), has coached the 7th and 8th grade football teams at St. Francis Xavier, is an active member of the Los Abogados Hispanic Bar Association, and serves meals to the homeless at Andre House in downtown Phoenix. John is also a member of Grand Canyon River Guides, and has been a licensed Grand Canyon River Guide for the past 22 years.</p>
+
+				<p>John devotes 100% of his career to fighting insurance companies and corporate interests on behalf of good people who are hurt. People call this “personal injury law.” John wears that term proudly. Although insurance companies, corporations, and their lobbyists want you to envision slick-backed hair, greedy, desperate lawyers running after an ambulance when you hear “personal injury lawyer.” John wants people to envision what it really is – taking vulnerable, broken, hurt people who are at the loneliest, darkest times in their life, and helping them rebuild – piece by piece. Having the backs of real human beings by fighting against greedy insurance companies and corporations who are looking to boost their ever-increasing profits on the broken backs of the people they hurt. That’s what a real “personal injury lawyer” is. And John is proud to be one.</p>
+
+				<h2>Areas of Practice</h2>
+
+				<p>John is 100% devoted to fighting insurance companies and corporations on behalf of good people who are hurt.</p>
+
+				<h2>Philosophy</h2>
+
+				<p>John devotes his practice to sticking up for those wronged by others. Then, holding the wrongdoer accountable. Whether his client is injured due to someone’s choice to disregard basic safety rules, or a corporation or insurance company’s attempt to take advantage of an individual, John relentlessly pursues justice for his clients through loyal advocacy, honest communication, and the committed pursuit of the truth. John works hard and creatively, drawing on his mind and his heart for his clients to ensure that they are treated fairly in every single step of the litigation process.</p>
+
+				<p>John gets his tireless work ethic from watching his dad, Fernando, report to work like clockwork as a Journeyman Lineman in Southern Utah for the past 43 years, then coming home and showing John how a real man works by being an amazing dad, coach, and maintaining their house. John’s mom, Karen, who raised him and his siblings to always stick up for themselves and anyone else getting picked on, gives him his unbending will to continue to take the fight to well-financed opponents, and seek justice for his clients.</p>
+				
+			</div><!-- att_bio_bottom -->
 			
 		</div><!-- att_bio_wrapper -->
 		
