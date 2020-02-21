bb835f3e891d34bfcddd388ebbdd1d584946d13f
Garrett Cullen <Garth619@users.noreply.github.com>
2020-02-20 17:50:40 -0800

    push

--- wp-content/themes/torgenson/page-templates/template-about.php
+++ wp-content/themes/torgenson/page-templates/template-about.php
@@ -14,7 +14,7 @@ get_header(); ?>
 			
 			<div class="about_top_content">
 				
-				<p>Our team of Phoenix personal injury lawyers will not stop fighting until we uncover every single detail that we need to know. Our investigative process is deeply thorough and effective, and we keep you included in every step of the process so you’re never in the dark. You can trust us to be completely honest with you and to continue defending your rights until the case has been closed. </p>
+				<?php the_field( 'top_section_intro_about' ); ?>
 				
 			</div><!-- about_top_content -->
 			
@@ -30,15 +30,7 @@ get_header(); ?>
 			
 			<div class="about_middle_left content">
 				
-				<h2>Get fully and fairly compensated</h2>
-
-				<p>While establishing economic losses that have already happened (like medical bills and property damage) is a relatively straightforward process, the same is not true for future losses or noneconomic injuries like physical and emotional pain or suffering. The assistance of an attorney will ensure that you get the compensation to which you are legally entitled.</p>
-
-				<p>If you mistakenly say something that indicates partial liability, it could significantly affect the amount of compensation you end up receiving. When you retain a lawyer, he or she will communicate with the insurance company or the adverse party on your behalf, ensuring that you do not say or do anything to hurt your case.</p>
-
-				<h2>Navigate tricky insurance negotiation</h2>
-
-				<p>Insurance companies are sophisticated parties that are in the business of denying claims and undoubtedly have an army of skilled insurance defense lawyers ready to litigate a case at the drop of a hat. As an accident victim, you need to prepared to take your case to trial, and Torgenson Law can help.</p>
+				<?php the_field( 'middle_section_content_about' ); ?>
 				
 			</div><!-- about_middle_left -->
 			
@@ -48,13 +40,27 @@ get_header(); ?>
 					
 					<div class="about_middle_img">
 						
-						<img src="<?php bloginfo('template_directory');?>/images/about-image1.jpg"/>
+						<?php $middle_section_image_one = get_field( 'middle_section_image_one' ); ?>
+						
+						<?php if ( $middle_section_image_one ) { ?>
+							
+							<img src="<?php echo $middle_section_image_one['url']; ?>" alt="<?php echo $middle_section_image_one['alt']; ?>" />
+						
+						<?php } ?>
+						
+					
 						
 					</div><!-- about_middle_img -->
 					
 					<div class="about_middle_img">
 						
-						<img src="<?php bloginfo('template_directory');?>/images/about-image2.jpg"/>
+						<?php $middle_section_image_two = get_field( 'middle_section_image_two' ); ?>
+						
+						<?php if ( $middle_section_image_two ) { ?>
+							
+							<img src="<?php echo $middle_section_image_two['url']; ?>" alt="<?php echo $middle_section_image_two['alt']; ?>" />
+						
+						<?php } ?>
 						
 					</div><!-- about_middle_img -->
 					
@@ -84,9 +90,9 @@ get_header(); ?>
 				
 				<div class="about_right_quote_wrapper">
 					
-					<p>“We are a team of trial lawyers born and bred to fight. When you are hurt, you need lawyers who will have your back every single step of the way. At Torgenson Law,we are 100% dedicated to fighting insurance companies and large corporations on behalf of good people. We have their back.”</p>
+					<p><?php the_field( 'bottom_section_quote_about' ); ?></p>
 					
-					<span class="about_right_quote_name">John Torgenson</span><!-- about_right_quote_name -->
+					<span class="about_right_quote_name"><?php the_field( 'bottom_section_name' ); ?></span><!-- about_right_quote_name -->
 					
 				</div><!-- about_right_quote_wrapper -->
 				
@@ -102,35 +108,32 @@ get_header(); ?>
 			
 			<div class="about_awards_slider">
 				
-				<div class="about_awards_slide">
-					
-					<div class="about_awards_inner">
-						
-						<img src="<?php bloginfo('template_directory');?>/images/award-1.jpg"/>
+				<?php if(get_field('awards_slider_about')): ?>
+				 
+					<?php while(has_sub_field('awards_slider_about')): ?>
 							
-					</div><!-- about_awards_inner -->
-					
-				</div><!-- about_awards_slide -->
-				
-				<div class="about_awards_slide">
-					
-					<div class="about_awards_inner">
-						
-						<img src="<?php bloginfo('template_directory');?>/images/awards-2.jpg"/>
 							
-					</div><!-- about_awards_inner -->
+							<div class="about_awards_slide">
 					
-				</div><!-- about_awards_slide -->
-				
-				<div class="about_awards_slide">
-					
-					<div class="about_awards_inner">
+								<div class="about_awards_inner">
 						
-						<img src="<?php bloginfo('template_directory');?>/images/award-3.jpg"/>
+									<?php $logos = get_sub_field( 'logos' ); ?>
+									
+									<?php if ( $logos ) { ?>
+										
+										<img src="<?php echo $logos['url']; ?>" alt="<?php echo $logos['alt']; ?>" />
+									
+									<?php } ?>
 							
-					</div><!-- about_awards_inner -->
+								</div><!-- about_awards_inner -->
 					
-				</div><!-- about_awards_slide -->
+							</div><!-- about_awards_slide -->
+
+						
+				    
+					<?php endwhile; ?>
+				 
+				<?php endif; ?>
 				
 			</div><!-- about_awards_slider -->
 			
