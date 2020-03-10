b0d3f6df547d2ed0169263ac7797138121e64d34
Garrett Cullen <Garth619@users.noreply.github.com>
2020-03-09 17:01:52 -0700

    push

--- wp-content/themes/torgenson/page-templates/homepage_template_parts/section-2.php
+++ wp-content/themes/torgenson/page-templates/homepage_template_parts/section-2.php
@@ -30,13 +30,13 @@
 			
 			<?php if(get_field('section_two_header')) { ?>
 		
-				<span class="sec_two_title">What to do if you are injured</span><!-- sec_two_title -->
+				<span class="sec_two_title"><?php the_field( 'section_two_bottom_header' ); ?></span><!-- sec_two_title -->
 			
 			<?php } ?>
 		
 			<div class="sec_two_intro">
 			
-				<p>For many who have been injured, the only resolution to regain financial control of their life is to pursue legal action. If you one day decide to file a personal injury lawsuit, it is important to know the initial steps that need to be taken in order to maintain the integrity of your claim.</p>
+				<?php the_field( 'section_two_bottom_intro' ); ?>
 						
 			</div><!-- sec_two_intro -->
 			
@@ -44,24 +44,13 @@
 			
 			<div class="sec_two_col">
 				
-				<h3>STEP #1- DOCUMENT WHAT HAPPENED</h2>
-				
-				<p>The more accurate and detailed you are able to document your injuries, the more likely you are to be fairly compensated for them. Keep a journal to accurately and specifically detail how your everyday life has been changed or altered as a result of your injuries. Include detailed descriptions of exactly what happened before, during, and after your injury. Also include job performance and wage loss details, emotional well-being, marital relations, and the ways in which your relationship with family and friends has been affected. Keep the personal contact information of any witnesses, as well as a running record of physician treatments and the dates of service for medical consulting.</p>
-
-				<h3>STEP #2- TAKE PHOTOS AND SECURE EVIDENCE</h3>
-				
-				<p>In securing evidence, your priority should be making sure that every item of physical evidence is preserved. Physical evidence may include broken or faulty equipment, torn clothing, and important documents. </p>
+				<?php the_field( 'section_two_column_one_bottom' ); ?>
 								
 			</div><!-- sec_two_col -->
 			
 			<div class="sec_two_col">
 				
-				<p>In this regard, be sure to keep or obtain all medical records pertaining to medical treatment you received as a result of your injury. Take detailed photographs of your injuries and the area where the injury occurred, from all angles, and if possible, under the same type of conditions.</p>
-
-				<h3>STEP #3- OBTAIN INCIDENT REPORT (IF POSSIBLE)</h3>
-				
-				<p>If you were involved in an incident in which a law enforcement officer responded, 
-it is more than likely that a police report was documented and filed. If the incident occurred on a commercial property such as a gym or grocery store, it is also likely that an incident report was generated. It is important to understand that you ARE entitled to receive copies of these documents. While the police and/or incident report may not be admitted into the civil court proceedings, it can lend a hand in attorney negotiations by providing leverage and clarity in your dispute.</p>
+				<?php the_field( 'section_two_column_two_bottom' ); ?>
 				
 			</div><!-- sec_two_col -->
 			
--- wp-content/themes/torgenson/page-templates/homepage_template_parts/section-4.php
+++ wp-content/themes/torgenson/page-templates/homepage_template_parts/section-4.php
@@ -110,40 +110,19 @@
 	
 	<div class="sec_four_bottom_content">
 		
-		<span class="sec_four_bottom_title">Don’t wait contact us today</span><!-- sec_four_bottom_title -->
+		<span class="sec_four_bottom_title"><?php the_field( 'section_four_bottom_header' ); ?></span><!-- sec_four_bottom_title -->
 		
 		<div class="sec_four_bottom_inner content">
 			
 			<div class="sec_four_col">
 				
-				<span class="h2">How Do I Prove Negligence in a Personal Injury Case?</span>
-
-				<p>Nearly all personal injury claims are based on a foundation of fault and negligence — and proving that one party’s negligence resulted in a preventable injury to you or your loved one. In the United States, there are four key elements to proving negligence:</p>
-				
-				<ul>
-				<li>Duty. There was a duty of care toward the plaintiff. The duty of care refers to the obligation to follow any proper rules in order to prevent the harm and/or damage to a person and/or his property.</li>
-				<li>Breach. There was a breach of duty toward the plaintiff. In other words, the defendant failed to uphold their duty of care toward the other party, causing or allowing a dangerous situation that led to serious injury.</li>
-				<li>Causation. The defendant’s breach directly caused the plaintiff’s injuries. The plaintiff must prove that the negligence of the other party was a primary cause for any resulting injuries.</li>
-				<li>Damages. The plaintiff suffered harm due to the defendant’s negligent actions.</li>
-				</ul>
-				
-				<p>Several circumstances surrounding each element can significantly complicate a negligence claim. For example, causation may be contested if there were multiple factors that led to you or your loved one’s injury. In such cases, an attorney is crucial to definitively proving negligence and securing the compensation you deserve.</p>
+				<?php the_field( 'section_four_column_one' ); ?>
 				
 			</div><!-- sec_four_col -->
 			
 				<div class="sec_four_col">
 					
-					<span class="h2">What Are the Statute of Limitations for Personal Injury Claims in Arizona?</span>
-
-					<p>Each state has laws in place that dictate the statute of limitations for personal injury claims — a time limit that injured parties must follow in order to file a valid personal injury claim.</p> 
-
-					<p>In the state of Arizona, plaintiffs generally have two years to file a personal injury claim. However, the point at which this clock begins can vary on a case by case basis. For some, it may begin on the date of the accident - but in certain circumstances, it may begin on the date you discover your injury.</p>
-
-					<p>It is always in your best interests to file your claim as soon as possible. As time passes, it gets more difficult to prove a personal injury case, as injuries heal and other substantiating evidence disappears or fades. Consulting with a personal injury attorney in a timely manner is your best chance at securing the compensation necessary to move forward with your life.</p>
-
-					<span class="h2">Contact Us Today</span>
-
-					<p>If you or a loved one has been seriously injured due to the negligence of another, please contact us immediately to discuss your case. We understand the significant impact of a serious injury on a person and his family, and we want to help you get back on your feet. Schedule a free initial consultation with us today and see how we can give you the support you deserve.</p>
+					<?php the_field( 'section_four_column_two' ); ?>
 					
 				</div><!-- sec_four_col -->
 			
